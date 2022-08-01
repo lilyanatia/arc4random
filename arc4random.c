e1bc6a04e2ba5c7143fac96224979696c080b251
Lily Wilson <hotaru@thinkindifferent.net>
2022-07-31 20:27:27 -0400

    misc cleanups

--- arc4random.c
+++ arc4random.c
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <sys/random.h>
 
+#define GETRANDOM_SIZE 256
+#define REKEY_INTERVAL 0x1000000
+
 #define KEYSTREAM_ONLY
 #include "chacha_private.h"
 #define KEYSZ 32
@@ -17,10 +20,11 @@ __attribute__((visibility("default"))) uint32_t arc4random(void)
 
 __attribute__((visibility("default"))) void arc4random_buf(void *buf, size_t nbytes)
 {
-  if(nbytes <= 256) while(getrandom(buf, nbytes, 0) < nbytes);
+  if(nbytes <= GETRANDOM_SIZE) while(getrandom(buf, nbytes, 0) < nbytes);
   else
   {
     unsigned char rnd[KEYSZ + IVSZ];
+    size_t length;
     chacha_ctx ctx;
     while(getrandom(rnd, KEYSZ + IVSZ, 0) < KEYSZ + IVSZ);
     while(nbytes)
@@ -28,9 +32,9 @@ __attribute__((visibility("default"))) void arc4random_buf(void *buf, size_t nby
       chacha_keysetup(&ctx, rnd, KEYSZ * 8, 0);
       chacha_ivsetup(&ctx, rnd + KEYSZ);
       chacha_encrypt_bytes(&ctx, rnd, rnd, KEYSZ + IVSZ);
-      chacha_encrypt_bytes(&ctx, buf, buf, nbytes > 0x1000000 ? 0x1000000 : nbytes);
-      buf += nbytes > 0x1000000 ? 0x1000000 : nbytes;
-      nbytes -= nbytes > 0x1000000 ? 0x1000000 : nbytes;
+      chacha_encrypt_bytes(&ctx, buf, buf, length = nbytes > REKEY_INTERVAL ? REKEY_INTERVAL : nbytes);
+      buf += length;
+      nbytes -= length;
     }
   }
 }
@@ -38,13 +42,12 @@ __attribute__((visibility("default"))) void arc4random_buf(void *buf, size_t nby
 __attribute__((visibility("default"))) uint32_t arc4random_uniform(uint32_t upper_bound)
  
 {
-  #define LENGTH 256
   const uint32_t limit = upper_bound < UINT32_MAX >> 1 ? UINT32_MAX - UINT32_MAX % upper_bound : upper_bound;
-  uint32_t out[LENGTH / sizeof(uint32_t)] = { 0 };
+  uint32_t out[GETRANDOM_SIZE / sizeof(uint32_t)] = { 0 };
   for(;;)
   {
-    while(getrandom(out, LENGTH, 0) < LENGTH);
-    for(int i = 0; i < LENGTH / sizeof(uint32_t); ++i)
+    while(getrandom(out, GETRANDOM_SIZE, 0) < GETRANDOM_SIZE);
+    for(int i = 0; i < GETRANDOM_SIZE / sizeof(uint32_t); ++i)
       if(out[i] < limit) return limit == upper_bound ? out[i] : out[i] % upper_bound;
   }
 }
