461d308d382d166876dee6b796e80e4804f0bd50
Lily Wilson <hotaru@thinkindifferent.net>
2022-08-03 23:16:41 -0400

    clean up arc4random_uniform

--- arc4random.c
+++ arc4random.c
@@ -26,13 +26,11 @@ __attribute__((visibility("default"))) void arc4random_buf(void *buf, size_t nby
 __attribute__((visibility("default"))) uint32_t arc4random_uniform(uint32_t upper_bound)
  
 {
-  const uint32_t limit = upper_bound < UINT32_MAX >> 1 ? UINT32_MAX - UINT32_MAX % upper_bound : upper_bound;
-  uint32_t out[GETRANDOM_SIZE / sizeof(uint32_t)] = { 0 };
+  uint32_t out, limit = UINT32_MAX - UINT32_MAX % upper_bound;
   for(;;)
   {
-    while(getrandom(out, GETRANDOM_SIZE, 0) < GETRANDOM_SIZE);
-    for(int i = 0; i < GETRANDOM_SIZE / sizeof(uint32_t); ++i)
-      if(out[i] < limit) return limit == upper_bound ? out[i] : out[i] % upper_bound;
+    while(getrandom(&out, sizeof(out), 0) < sizeof(out) || out >= limit);
+    return out % upper_bound;
   }
 }
 
