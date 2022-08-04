11600823289b6d42e71c9d5909d608089c263a26
Lily Wilson <hotaru@thinkindifferent.net>
2022-08-04 07:24:16 -0400

    remove arc4random_double_buf and arc4random_float_buf

--- arc4random.h
+++ arc4random.h
@@ -5,7 +5,3 @@ uint32_t arc4random(void);
 void arc4random_buf(void *buf, size_t nbytes);
 
 uint32_t arc4random_uniform(uint32_t upper_bound);
-
-void arc4random_double_buf(double *buf, size_t length);
-
-void arc4random_float_buf(float *buf, size_t length);
