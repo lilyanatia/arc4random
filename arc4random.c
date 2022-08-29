b07018c1a5be722b05cdb8f2b4696d65835f4a58
Lily Wilson <hotaru@thinkindifferent.net>
2022-08-29 18:02:20 -0400

    compatibility with legacy systems (glibc <2.25, FreeBSD <12, NetBSD <10, OpenBSD, Windows, etc.)

--- arc4random.c
+++ arc4random.c
@@ -2,7 +2,17 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <sys/param.h>
+#if defined(USE_GETENTROPY)
+#include <unistd.h>
+#elif defined(USE_BCRYPTGENRANDOM)
+#include <bcrypt.h>
+#elif defined(USE_CRYPTGENRANDOM)
+#include <wincrypt.h>
+#elif defined(USE_URANDOM)
+#include <stdio.h>
+#else
 #include <sys/random.h>
+#endif
 #include <sys/types.h>
 
 #if !defined(__GNUC__) || __GNUC__ < 3
@@ -11,6 +21,37 @@
 
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #define likely(x)   __builtin_expect(!!(x), 1)
+
+#if defined(USE_GETENTROPY)
+static inline ssize_t getrandom(void *buf, size_t buflen, unsigned int flags)
+{
+  if(buflen > 256) buflen = 256;
+  return getentropy(buf, buflen) || buflen;
+}
+#elif defined(USE_BCRYPTGENRANDOM)
+static inline ssize_t getrandom(void *buf, size_t buflen, unsigned int flags)
+{
+  return likely(BcryptGenRandom(NULL, buf, buflen, BCRYPT_USE_SYSTEM_PREFERRED_RNG)) ?
+         buflen : -1;
+}
+#elif defined(USE_CRYPTGENRANDOM)
+static inline ssize_t getrandom(void *buf, size_t buflen, unsigned int flags)
+{
+  HCRYPTPROV hCryptProv;
+  return likely(CryptAcquireContext(&hCryptProv, NULL, (LPCWSTR)L"Microsoft Base Cryptographic Provider v1.0", PROV_RSA_FULL, CRYPT_VERIFYCONTEXT) &&
+                CryptGenRandom(hCryptProv, buflen, buf)) ?
+         buflen : -1;
+}
+#elif defined(USE_URANDOM)
+static inline ssize_t getrandom(void *buf, size_t buflen, unsigned int flags)
+{
+  FILE *urandom = fopen("/dev/urandom", "r");
+  if(unlikely(!urandom)) return -1;
+  ssize_t length = fread(buf, 1, MIN(SSIZE_MAX, buflen), urandom);
+  fclose(urandom);
+  return length;
+}
+#endif
 
 #define GETRANDOM(r) while(unlikely(getrandom(&r, sizeof(r), 0) < 0))
 
